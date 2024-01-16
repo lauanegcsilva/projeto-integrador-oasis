31ee6933ba2c987b215d517df66e2a43eb2b0d69
Lauane Gonçalves <lauane.gcsilva@gmail.com>
2024-01-16 15:03:16 -0300

    feat: adicionam a model com arquivo categoria

---
+++ oasis/src/main/java/com/generation/oasis/model/Categoria.java
@@ -0,0 +1,45 @@
+﻿package com.generation.oasis.model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+    
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) 
+	private Long id;
+	
+    @NotNull
+	private String tipo;
+    
+    @UpdateTimestamp
+	private LocalDateTime data;
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	 }
