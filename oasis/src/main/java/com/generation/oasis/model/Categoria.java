752360e3c23f61f5e4a845588c6ab8a3e8f8aca0
Lauane Gonçalves <lauane.gcsilva@gmail.com>
2024-01-19 15:34:09 -0300

    fix: tira o atributo data da Categoria

--- oasis/src/main/java/com/generation/oasis/model/Categoria.java
+++ oasis/src/main/java/com/generation/oasis/model/Categoria.java
@@ -1,13 +1,16 @@
 ﻿package com.generation.oasis.model;
 
-import java.time.LocalDateTime;
+import java.util.List;
 
-import org.hibernate.annotations.UpdateTimestamp;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 
@@ -22,8 +25,9 @@ public class Categoria {
 	@NotNull
 	private String tipo;
 
-	@UpdateTimestamp
-	private LocalDateTime data;
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
 
 	public Long getId() {
 		return id;
@@ -40,4 +44,15 @@ public class Categoria {
 	public void setTipo(String tipo) {
 		this.tipo = tipo;
 	}
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
+	
+	
+
 }
